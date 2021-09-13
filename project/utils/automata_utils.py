231d2d4473294b65b4c6ce4a84f50ad20e7fbc65
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2021-09-13 15:52:29 +0300

    [TASK 2] Added more tests, created AutomataException class

--- project/utils/automata_utils.py
+++ project/utils/automata_utils.py
@@ -1,5 +1,14 @@
 from pyformlang.finite_automaton import DeterministicFiniteAutomaton
-from pyformlang.regular_expression import Regex
+from pyformlang.regular_expression import Regex, MisformedRegexError
+
+
+class AutomataException(Exception):
+    """
+    Base exception for automata utils
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
 
 
 def transform_regex_to_dfa(regex_str: str) -> DeterministicFiniteAutomaton:
@@ -18,7 +27,11 @@ def transform_regex_to_dfa(regex_str: str) -> DeterministicFiniteAutomaton:
         Minimal DFA built on given regular expression
     """
 
-    regex = Regex(regex_str)
+    try:
+        regex = Regex(regex_str)
+    except MisformedRegexError as e:
+        raise AutomataException(f"Invalid regular expression")
+
     enfa = regex.to_epsilon_nfa()
 
     return enfa.minimize()
